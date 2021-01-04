8380424146066b9dc7b904b07d2ebfa040b61847
avgupt <avishig012@gmail.com>
2021-01-04 18:58:04 +0530

    Add decorator tag

--- OOP/concepts/decorator.py
+++ OOP/concepts/decorator.py
@@ -9,7 +9,13 @@ def decorate_divide(func):
 def divide(x, y):
 	return x // y
 
+@decorate_divide
+def divide1(x, y):
+	return x // y
+
 decorated_divide = decorate_divide(divide)
 
 print(decorated_divide(6, 3))
 print(decorated_divide(2, 0))
+print(divide1(24, 4))
+print(divide1(9, 0))
